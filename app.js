6cbb34e8d5f079920954ee50180289e0be6ff6ac
TimW010 <0981877@hr.nl>
2021-03-15 19:42:04 +0100

    Merge branch 'main' of https://github.com/Amece/CLE3-Baroeg into main

--- app.js
+++ app.js
@@ -13,9 +13,54 @@ const client = new tmi.Client({
 	channels: [process.env.TWITCH_USERNAME]
 });
 client.connect().catch(console.error);
-client.on('message', (channel, tags, message, self) => {
-    console.log(channel, message);
-	//if(message.toLowerCase() === '!hello') {
-	//	client.say(channel, `@${tags.username}, heya!`);
-	//}
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+client.on('message', (channel, userstate, message, self) => {
+	//uncomment this to remove the chat filter for the host.
+	//if(self) return;
+	//if(userstate.username === process.env.TWITCH_USERNAME) return;
+
+	filterChat(userstate, message, channel);
 });
+
+// Called every time a message comes in
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
+
+  // If the command is known, let's execute it
+  if (commandName === '!dice') {
+    const num = rollDice();
+    client.say(target, `You rolled a ${num}`);
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
+}
+
+// Function called when the "dice" command is issued
+function rollDice () {
+  const sides = 6;
+  return Math.floor(Math.random() * sides) + 1;
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
+
+function filterChat(userstate, message, channel) {
+	const blockedWords = ['test'];
+
+	message = message.toLowerCase();
+	let shouldSendMessage = false;
+	shouldSendMessage = blockedWords.some(blockedWord => message.includes(blockedWord.toLowerCase()));
+	if (shouldSendMessage) {
+		// tells user
+		client.say(channel, `@${userstate.username} The message you typed was inappropriate and got deleted.`)
+		// delete message
+		client.deletemessage(channel, userstate.id)
+	}
+}
